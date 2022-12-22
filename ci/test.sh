0e8fc9897f78d80fed5800f1eefda35fbe33c467
Paul <22284856+ProgramComputer@users.noreply.github.com>
2022-12-22 16:45:06 +0000

    removed

---
+++ ci/test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export GO111MODULE="on"
+go test -tags 'watch' -v -race -coverprofile=coverage.txt -covermode=atomic -coverpkg github.com/ProgramComputer/plugin-xdcc
+go install golang.org/x/vuln/cmd/govulncheck@latest
+"$(go env GOPATH)"/bin/govulncheck -tags production ./...
